ea375b14c1924a89b617df6192494fe50925e66e
Dmitrii <atheros91@yandex.ru>
2020-03-16 13:31:39 +0400

    DNDAPP-4 Use Firebase Firestore DB for characters.

--- app/src/main/java/com/dndapp/character/list/CharacterListAdapter.kt
+++ app/src/main/java/com/dndapp/character/list/CharacterListAdapter.kt
@@ -3,11 +3,12 @@ package com.dndapp.character.list
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import com.dndapp.databinding.ViewCharacterItemBinding
+import com.dndapp.model.Document
 import com.dndapp.model.character.Character
 import com.dndapp.utils.BindableViewHolder
 import com.dndapp.utils.GenericAdapter
 
-class CharacterListAdapter : GenericAdapter<String, Character, CharacterViewHolder>() {
+class CharacterListAdapter : GenericAdapter<String, Document<Character>, CharacterViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CharacterViewHolder {
         val binding = ViewCharacterItemBinding.inflate(
             LayoutInflater.from(parent.context), parent, false
@@ -18,9 +19,9 @@ class CharacterListAdapter : GenericAdapter<String, Character, CharacterViewHold
 
 class CharacterViewHolder(
     private val binding: ViewCharacterItemBinding
-) : BindableViewHolder<String, Character>(binding.root) {
-    override fun bind(item: Character) {
-        binding.character = item
+) : BindableViewHolder<String, Document<Character>>(binding.root) {
+    override fun bind(item: Document<Character>) {
+        binding.character = item.data
         binding.executePendingBindings()
     }
 }
