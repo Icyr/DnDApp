2a452066fdd05a131ebe0d5c53058e08314bfe28
Va Ko <dndappb412d@gmail.com>
2020-04-19 19:23:11 +0400

    https://github.com/Icyr/DnDApp/issues/12

--- app/src/main/java/com/dndapp/character/list/CharacterListViewModel.kt
+++ app/src/main/java/com/dndapp/character/list/CharacterListViewModel.kt
@@ -1,8 +1,13 @@
 package com.dndapp.character.list
 
+import androidx.databinding.BaseObservable
+import androidx.databinding.Bindable
 import androidx.lifecycle.ViewModel
+import com.dndapp.BR
 import com.dndapp.R
+import com.dndapp.landing.signin.SignInState
 import com.dndapp.model.character.CharacterRepository
+import com.dndapp.utils.BaseObservableLiveData
 import com.dndapp.viewmodel.Destination
 import com.dndapp.viewmodel.NavigationViewModel
 
@@ -11,7 +16,21 @@ class CharacterListViewModel(
     private val navigationViewModel: NavigationViewModel
 ) : ViewModel() {
 
+
+    val state = BaseObservableLiveData(CharacterListState())
+
     val characters = characterRepository.getCharacters()
 
     fun onCreate() = navigationViewModel.navigate(Destination(R.id.fragment_character_create))
+
+
+    class CharacterListState : BaseObservable() {
+
+        @get:Bindable
+        var loading: Boolean = false
+            set(value) {
+                field = value
+                notifyPropertyChanged(BR.loading)
+            }
+    }
 }
