e39b8298ab85cbfdaf29e626f33c06deea7573b1
Va Ko <dndappb412d@gmail.com>
2020-05-20 18:58:14 +0400

    Add abilitiy scores #27

--- app/src/main/java/com/dndapp/model/character/room/JoinedCharacter.kt
+++ app/src/main/java/com/dndapp/model/character/room/JoinedCharacter.kt
@@ -2,7 +2,6 @@ package com.dndapp.model.character.room
 
 import androidx.room.Embedded
 import androidx.room.Relation
-import com.dndapp.model.abilityscores.AbilityScores
 import com.dndapp.model.abilityscores.room.AbilityScoresEntity
 import com.dndapp.model.background.room.BackgroundEntity
 import com.dndapp.model.characterClass.room.CharacterClassEntity
@@ -12,6 +11,7 @@ data class JoinedCharacter(
     @Embedded val character: CharacterEntity,
     @Relation(parentColumn = "race_id", entityColumn = "id") val race: RaceEntity,
     @Relation(parentColumn = "background_id", entityColumn = "id") val background: BackgroundEntity,
-    @Relation(parentColumn = "character_class_id", entityColumn = "id") val characterClass: CharacterClassEntity,
-    @Embedded(prefix = "ability_scores_id") val abilityScores: AbilityScoresEntity
+    @Relation(parentColumn = "character_class_id", entityColumn = "id") val characterClass: CharacterClassEntity
+    //,
+   // @Embedded(prefix = "ability_scores_id_") val abilityScores: AbilityScoresEntity
 )
